dbf814ec659a05910fd3e14997a4fc095393b2c7
encheta <149392499+encheta@users.noreply.github.com>
2023-11-27 21:20:53 +0800

    Update install.sh
    Updated so that script should get to compile on userland.. Can't guarantee anything else yet though!

--- install.sh
+++ install.sh
@@ -5,16 +5,13 @@ sudo apt-get -y install software-properties-common
 sudo wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add - #
 sudo add-apt-repository 'deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-16 main'
 sudo apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential libomp-dev git screen nano jq wget
-sudo apt-get -y install libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64
+sudo apt-get -y install libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64
 sudo ln -sf /usr/lib/llvm-16/bin/clang-16 /usr/bin/clang
 sudo ln -sf /usr/lib/llvm-16/bin/clang++ /usr/bin/clang++
-wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 
-if [ ! -d ~/CCminer-ARM-optimized ]
+if [ ! -d ~/CCminer ]
 then
-  mkdir ~/CCminer-ARM-optimized
+  mkdir ~/CCminer
 fi
 
 git clone https://github.com/simeononsecurity/CCminer-ARM-optimized.git
@@ -23,8 +20,10 @@ chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 CXX=clang++ CC=clang ./build.sh
+sudo make install
 
-cd ~/CCminer-ARM-optimized
+cd
+cd ~/CCminer
 wget https://raw.githubusercontent.com/encheta/Android-Mining-ARMv8/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
